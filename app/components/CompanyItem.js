7041d0c2d1b0ee50aecb00e9600cd07d7d294618
saki <16546851+sakiko-2@users.noreply.github.com>
2019-12-17 03:59:22 +1300

    Add company list

---
+++ app/components/CompanyItem.js
@@ -0,0 +1,69 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { getTheme } from 'react-native-material-kit';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const theme = getTheme();
+
+const CompanyItem = (props) => {
+  const { company, firstName, lastName } = props.companies;
+
+  return (
+    <View style={[theme.cardStyle, styles.card]}>
+      <Icon
+        name="business"
+        size={50}
+        style={styles.icon}
+      />
+        <Text style={[theme.cardTitleStyle, styles.title]}>{company}</Text>
+        <Text style={[theme.cardActionStyle, styles.action]}>
+          {firstName} {lastName}
+        </Text>
+    </View>
+  );
+};
+
+CompanyItem.propTypes = {
+  companies: PropTypes.shape({
+    company: PropTypes.string,
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+  }),
+};
+
+const styles = StyleSheet.create({
+  card: {
+    marginTop: 15,
+    marginRight: 15,
+    marginLeft: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 1.0,
+    shadowRadius: 5,
+    elevation: 1,
+  },
+  title: {
+    fontSize: 20,
+    top: 12,
+    left: 60,
+    width: 310,
+  },
+  action: {
+    color: '#fff',
+    backgroundColor: '#000',
+  },
+  icon: {
+    top: 5,
+    left: 0,
+    paddingTop: 17,
+    paddingBottom: 22,
+    paddingLeft: 10,
+  },
+});
+
+export default CompanyItem;
--- app/components/CompanyList.js
+++ app/components/CompanyList.js
@@ -1,15 +1,25 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import {
-  Text,
+  FlatList,
+  StyleSheet,
   View,
 } from 'react-native';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/EvilIcons';
-import * as actions from '../actions';
+import CompanyItem from './CompanyItem';
 
 const CompanyList = (props) => (
-  <View>
-    <Text>Add company screen</Text>
+  <View style={styles.containerStyles}>
+    <FlatList
+      data={props.contacts.map((value, key) => ({
+        company: value.company,
+        firstName: value.firstName,
+        lastName: value.lastName,
+      })).sort((a, b) => a.company - b.company)}
+      renderItem={({ item }) => <CompanyItem companies={item} />}
+      keyExtractor={(item, index) => index.toString()}
+    />
   </View>
 );
 
@@ -19,4 +29,21 @@ CompanyList.navigationOptions = {
   )
 };
 
-export default connect(null, actions)(CompanyList);
+CompanyList.propTypes = {
+  contacts: PropTypes.array,
+};
+
+const styles = StyleSheet.create({
+  containerStyles: {
+    flex: 1,
+    flexWrap: 'wrap',
+    backgroundColor: '#fff',
+    paddingTop: 25,
+  },
+});
+
+const mapStateToProps = (state) => ({
+  contacts: state.contacts,
+});
+
+export default connect(mapStateToProps)(CompanyList);
